763f9307f01fac8872ca622a3981e4b3616212e2
marnixah <marnix@hagehoeve.com>
2022-02-23 14:52:53 +0100

    fix(welcome): same styling as dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 <x-app-layout>
-    <div class="max-w-7xl bg-white mx-auto rounded-xl py-3 px-2 mt-4">
-        This website is a frontend for downloading audio files from <a href="https://japaneseasmr.com" class="text-blue-500 hover:text-blue-700">japaneseasmr.com</a>. It provides an easy way to download files by simply adding them in the dashboards "Download" column.
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    This website is a frontend for downloading audio files from <a href="https://japaneseasmr.com" class="text-blue-500 hover:text-blue-700">japaneseasmr.com</a>. It provides an easy way to download files by simply adding them in the dashboards "Download" column.
+                </div>
+            </div>
+        </div>
     </div>
 </x-app-layout>
